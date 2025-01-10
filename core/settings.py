2e9c579118e5bb4125e046003a3fa9ddf7389e53
bbars abbos <abbosbek.info@gmail.com>
2025-01-10 16:16:05 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-!4a(vh&r@&v%+^t$(efr3wnwv%%xgmqut(q7-*^%8n06oafz$e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
